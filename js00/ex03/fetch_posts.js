b5f6a566d6347c2c3f9e9481481400e79f3782e3
dujamess <duja.dija.12@gmail.com>
2025-09-26 18:48:57 +0100

    ex03 done

---
+++ js00/ex03/fetch_posts.js
@@ -0,0 +1,21 @@
+async function Data()
+{
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    try{
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`${response.status}`)
+        }
+        const l = await response.json()
+        console.log(l.length)
+        let s = l.length -10;
+        for(;s< l.length;s++)
+            console.log(l[s].title)
+    }
+    catch(error){
+
+        console.error("Error is : ",error.message)
+    }
+
+}
+Data()
